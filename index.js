dc010b3584f047ad045040827b15c52e609f6eab
YojiSato65 <yojisato65@gmail.com>
2022-07-01 20:17:01 +0300

    adds final verision

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const listItem = document.getElementsByClassName('right-div')
+const listItem = document.querySelector('.right-div')
+const spinner = document.querySelector('.spinner-div')
 
 async function fetchItems()
 {
@@ -9,31 +10,43 @@ async function fetchItems()
         {
             const data = await response.json()
             console.log(data.result);
-        }
 
-        listItem.innerHTML = data.result.map(item => `
-    <div><img src="${item.image}" alt="NFT image"></div>
-            <div>
-                <div>
-                    <a href="">${item.collectionName}</a>
-                    <a href="">${item.name}</a>
-                </div>
-                <div>
-                    <div>BUY NOW</div>
-                    <a href="">${item.price}</a>
-                </div>
-                <hr>
-                <div>Utility type</div>
-                <a href="">${item.utilityType}</a>
+            listItem.innerHTML = data.result.map(item => `
+        <div class='item-div'>
+            <div class='item-image-div'>
+                <img src="${item.image.includes('seadn.io') ? item.image : null}" alt="NFT image">
             </div>
-    `).join('')
-
+            <div class='item-description-div'>
+                <div class='item-name-div'>
+                    <div>
+                        <p>${item.collectionName}</p>
+                        <i class="fa-regular fa-circle-check"></i>
+                    </div>
+                    <h5>${item.name}</h5>
+                </div>
+                <div class='item-price-div'>
+                    <p>BUY NOW</p>
+                    <h5>${item.price * 100000 > 5 ? item.price.toFixed(3) : item.price.toFixed(5)} ETH</h5>
+                </div>
+            </div>
+                <hr>
+            <div class='item-utility-div'>
+                <p>Utility type</p>
+                <h5>${item.utilityType}</h5>
+            </div>
+        </div>
+        `).join('')
+        }
     } catch (error)
     {
         console.log('error', error);
+        alert('Something went wrong. Please reload the page.')
+    } finally
+    {
+        spinner.classList.add('d-none')
     }
 
 
 }
-window.onload = fetchItems
 
+window.onload = fetchItems
