e9f471ff9b14cc7eb895d953a9f80f47895fa6d8
YojiSato65 <yojisato65@gmail.com>
2022-07-06 19:21:48 +0300

    adds input number clickable

--- index.js
+++ index.js
@@ -1,62 +1,10 @@
-// const listItems = document.querySelector('.right-div')
-// const spinner = document.querySelector('.spinner-div')
-
-// async function fetchItems()
-// {
-//     try
-//     {
-//         const response = await fetch('https://mdatsev.dev/token-api?offset=0&limit=10')
-//         if (response.ok)
-//         {
-//             const data = await response.json()
-//             console.log(data.result);
-
-//             listItems.innerHTML = data.result.map(item => `
-//         <div class='item-div'>
-//             <div class='item-image-div'>
-//                 <img src="${item.image.includes('seadn.io') ? item.image : null}" alt="NFT image">
-//             </div>
-//             <div class='item-description-div'>
-//                 <div class='item-name-div'>
-//                     <div>
-//                         <p>${item.collectionName}</p>
-//                         <i class="fa-regular fa-circle-check"></i>
-//                     </div>
-//                     <h5>${item.name}</h5>
-//                 </div>
-//                 <div class='item-price-div'>
-//                     <p>BUY NOW</p>
-//                     <h5>${item.price * 100000 > 5 ? item.price.toFixed(3) : item.price.toFixed(5)} ETH</h5>
-//                 </div>
-//             </div>
-//                 <hr>
-//             <div class='item-utility-div'>
-//                 <p>Utility type</p>
-//                 <h5>${item.utilityType}</h5>
-//             </div>
-//         </div>
-//         `).join('')
-//         }
-//     } catch (error)
-//     {
-//         console.log(error.message);
-//         alert('An error occurred. Click OK to reload the page.')
-//     } finally
-//     {
-//         spinner.classList.add('d-none')
-//     }
-// }
-
-// window.onload = fetchItems
-
-
 
 (function ()
 {
     const listItems = document.querySelector('.item-list-div')
     const spinner = document.querySelector('.infinite-spinner-div')
 
-    const getItems = async (page, limit) =>
+    const getItems = async () =>
     {
         const API_URL = 'https://mdatsev.dev/token-api?offset=0&limit=10';
         const response = await fetch(API_URL);
@@ -70,15 +18,17 @@
 
     const showItems = (items) =>
     {
+
+        console.log(items);
         items.map(item =>
         {
-            const itemEl = document.createElement('div');
-            itemEl.classList.add('item-div');
+            const itemDiv = document.createElement('div');
+            itemDiv.classList.add('item-div');
 
-            itemEl.innerHTML = `
+            itemDiv.innerHTML = `
             <div class='item-image-div'>
-                <img src="${item.image.includes('seadn.io') ? item.image : null}" alt="NFT image">
-            </div>
+                <img id='img' src="${item.image}" alt="NFT image" >
+            </div >
             <div class='item-description-div'>
                 <div class='item-name-div'>
                     <div>
@@ -89,17 +39,17 @@
                 </div>
                 <div class='item-price-div'>
                     <p>BUY NOW</p>
-                    <h5>${item.price * 100000 > 5 ? item.price.toFixed(3) : item.price.toFixed(5)} ETH</h5>
-                </div>
-            </div>
-                <hr>
+                    <h5>${item.price * 100000 > 5 ? item.price.toFixed(3) : item.price.toFixed(5)} ETH</h5 >
+                </div >
+            </div >
+        <hr>
             <div class='item-utility-div'>
                 <p>Utility type</p>
                 <h5>${item.utilityType}</h5>
             </div>
-        `;
+            `;
 
-            listItems.appendChild(itemEl);
+            listItems.appendChild(itemDiv);
         });
     };
 
@@ -124,30 +74,19 @@
         } catch (error)
         {
             console.log(error.message);
-            console.log('will try fetch again');
             hideLoader()
             loadItems()
         } finally
         {
             hideLoader();
         }
-
     };
 
-    window.addEventListener('scroll', () =>
-    {
-        const {
-            scrollTop,
-            scrollHeight,
-            clientHeight
-        } = document.documentElement;
-
-        loadItems();
-    }, {
-        passive: true
-    });
+    window.addEventListener('scroll', () => { loadItems() }, { passive: true }
+    );
 
     // initialize
     loadItems();
-
 })();
+
+
